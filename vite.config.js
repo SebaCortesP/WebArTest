17be140d1db53942b2f7e1560848d48254208b91
SebaCortesP <seba.cortesp@duocuc.cl>
2025-06-01 12:29:07 -0400

    ff

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://sebacortesp.github.io/WebArTest/',
+  // base: 'https://sebacortesp.github.io/WebArTest/',
+  base: '/WebArTest/',
   plugins: [
     vue(),
     vueDevTools(),
